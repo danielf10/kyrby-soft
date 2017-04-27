8f4ece148792dd388fd899922f69f9fe87a10754
danielf10 <androdan54@gmail.com>
2017-04-27 14:41:20 -0500

    proyecto unido2

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -14,7 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+//ndfhsjkdhfjkdshfjkdhsfjkhdsjkfhdsjkhfjdkshkjdshkjd
 /**
  *
  * @author jonathan
